e3eb6596d10e308122f23ed64207e9287cad236f
Iurii Lopatenko <yalopatenko@gmail.com>
2024-05-16 18:54:19 -0700

    .env file and env_file in docker compose file

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
    res.status(200).json({ message: 'Hi There! This is express server!' });
 });
 app.listen(port, () => {
-   console.log(`Server started on port ${port}! Connection string is ${connectionString}`);
+   console.log(`Server started on port ${port}!`);
 });
